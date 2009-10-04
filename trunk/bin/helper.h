c22cda0b20291e066ffbff02facf81b0b61bd7e6
Nguyen.Phuc.X <Nguyen.Phuc.X@6a860f62-a4c3-11de-afd7-c7ca60a6e2ec>
2009-10-04 21:49:18 +0000

    Added helper.h

--- trunk/bin/helper.h
+++ trunk/bin/helper.h
@@ -26,10 +26,6 @@ int handlelogin(char* name,int sock){
   unsigned char * payload_c = (unsigned char*) payload;
 
   int j;
-  /* for(j = 0; j < 12; j++){ */
-/*     printf("%02x ", payload_c[j]); */
-/*   } */
-
   printf("\n");
 
   unsigned char * header_c = (unsigned char *) hdr;
@@ -50,14 +46,6 @@ int handlelogin(char* name,int sock){
     }
   }
 
-  /* for(j = 0; j < 16; j++){ */
-/*     printf("%02x ", tosent[j]); */
-/*   } */
-
-  /* printf("\n"); */
-
-/*   printf("tosent: [%02x]\n", *tosent); */
-
   // Send a login message to the server
   int bytes_sent = send(sock, tosent,16,0);
   if (bytes_sent < 0) {
@@ -72,39 +60,39 @@ int handlelogin(char* name,int sock){
 }
 
 int handlemove(char * direction, int sock){
-  struct header *msg = (struct header *) malloc(sizeof(int)*2);
-  struct move * move = (struct move *) malloc(sizeof(int));
-  msg->version = 0x04;
-  msg->len = 0x08;
-  msg->msgtype = MOVE;
-  msg->payload[0] = 0x0;
+  /* struct header *hdr = (struct header *) malloc(sizeof(int)*2); */
+/*   struct move * move = (struct move *) malloc(sizeof(int)); */
 
-  unsigned char d;
+/*   hdr->version = 0x04; */
+/*   hdr->len = 0x08; */
+/*   hdr->msgtype = MOVE; */
 
-  if(strcmp(direction,"north")==0){
-    d = 0;
-  }else if(strcmp(direction,"south")==0){
-    d = 1;
-  }else if(strcmp(direction,"east")==0){
-    d = 2;
-  }else if(strcmp(direction,"west")==0){
-    d = 3;
-  }
+/*   unsigned char d; */
 
-  move->direction = d;
-  //  move->padding = 0x0;
+/*   if(strcmp(direction,"north")==0){ */
+/*     d = 0; */
+/*   }else if(strcmp(direction,"south")==0){ */
+/*     d = 1; */
+/*   }else if(strcmp(direction,"east")==0){ */
+/*     d = 2; */
+/*   }else if(strcmp(direction,"west")==0){ */
+/*     d = 3; */
+/*   } */
 
-  msg->payload = (char*) move;
+/*   move->direction = d; */
+/*   //  move->padding = 0x0; */
+
+/*   hdr->payload = (char*) move; */
   
-  char* tosent = (char*) msg;
+/*   char* tosent = (char*) hdr; */
   
-  int bytes_sent = send(sock,tosent,msg->len,0);
-  if (bytes_sent < 0){
-    perror("send failed.\n");
-  } else {
-    printf("Send: %d bytes\n", bytes_sent);
-  }
-  return 0;
+/*   int bytes_sent = send(sock,tosent,hdr->len,0); */
+/*   if (bytes_sent < 0){ */
+/*     perror("send failed.\n"); */
+/*   } else { */
+/*     printf("Send: %d bytes\n", bytes_sent); */
+/*   } */
+/*   return 0; */
 }
 
 
@@ -114,79 +102,45 @@ int handleattack(char * victim, int sock){
 
   // Logic
 
-  // Check if the victim is out of sight
-  // Search for the guy
+/*   // Check if the victim is out of sight */
+/*   // Search for the guy */
   
 
 
-  // Sending message
-  struct header *msg = (struct header *) malloc(sizeof(int)*2);
-  struct attack * attack = (struct move *) malloc(sizeof(int));
+/*   // Sending message */
+/*   struct header *hdr = (struct header *) malloc(sizeof(int)*2); */
+/*   struct attack * attack = (struct move *) malloc(sizeof(int)); */
 
-  msg->version = 0x04;
-  msg->len = 0x08;
-  msg->msgtype = ATTACK;
-  msg->payload[0] = 0x0;
-  msg->payload[1] = 0x0;
-  msg->payload[2] = 0x0;
+/*   hdr->version = 0x04; */
+/*   hdr->len = 0x08; */
+/*   hdr->hdrtype = ATTACK; */
 
-  // Copy the name
-  strcpy(attack->victimname,victim);
+/*   // Copy the name */
+/*   strcpy(attack->victimname,victim); */
 
-  // Cast the payload to char*
-  msg->payload = (char*) attack;
+/*   // Cast the payload to char* */
 
-  // Cast the message to tosent
-  char * tosent = (char*) msg;
+/*   // Cast the message to tosent */
+/*   char * tosent = (char*) hdr; */
 
-  // Sending
-  int bytes_sent = send(sock,tosent,msg->len,0);
-  if (bytes_sent < 0){
-    perror("send failed.\n");
-    return -1;
-  } else {
-    printf("Send: %d bytes\n", bytes_sent);
-  }
-  return 0;
+/*   // Sending */
+/*   int bytes_sent = send(sock,tosent,hdr->len,0); */
+/*   if (bytes_sent < 0){ */
+/*     perror("send failed.\n"); */
+/*     return -1; */
+/*   } else { */
+/*     printf("Send: %d bytes\n", bytes_sent); */
+/*   } */
+/*   return 0; */
 }
 
 // Speak
 int handlespeak(char * m, int sock){
-  int total_length = strlen(m);
-
-  // Sending message
-  struct header *msg = (struct header *) malloc(sizeof(int)*2);
-  struct speak * speak = (struct speak *) malloc(sizeof(int));
-
-  msg->version = 0x04;
-  msg->len = 0x08;
-  msg->msgtype = SPEAK;
-
-  // Copy the name
-  strcpy(speak->msg,m);
-
-  // Cast the payload to char*
-  msg->payload = (char*) speak;
-
-  // Cast the message to tosent
-  char * tosent = (char*) msg;
-
-  // Sending
-  int bytes_sent = send(sock,tosent,msg->len,0);
-  if (bytes_sent < 0){
-    perror("send failed.\n");
-    return -1;
-  } else {
-    printf("Send: %d bytes\n", bytes_sent);
-  }
-
-  free(msg);
-  free(speak);
-  return 0;
 }
 
 int handlelogout(char * name,int sock){
-  struct header *msg = (struct header *) malloc(sizeof(int));
+  struct header *hdr = (struct header *) malloc(sizeof(int));
+
   msg->version = 0x04;
   msg->len = 0x08;
   msg->msgtype = LOGOUT;
