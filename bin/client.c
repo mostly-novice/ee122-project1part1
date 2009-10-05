650757e3ef61a4f743e42cc5f4ba5bde9a820586
Nguyen.Phuc.X <Nguyen.Phuc.X@6a860f62-a4c3-11de-afd7-c7ca60a6e2ec>
2009-10-05 09:19:44 +0000

    added speak, speak notify, attack, attack notify, and the awesomeness of (or lack there of) of tony)

--- bin/client.c
+++ bin/client.c
@@ -11,12 +11,13 @@
 #include "header.h"
 #include "helper.h"
 #include "messages.h"
+#include "aux.h"
 
 #define STDIN 0
 #define HEADER_LENGTH 4
 
 typedef struct P{
-  char * name;
+  char name[10];
   int hp;
   int exp;
   int x;
@@ -40,12 +41,21 @@ Node * findPlayer(char * name, Node * list){
   return NULL;
 }
 
+Node * addPlayer(char * name, Node * list){
+  // Add player
+}
+
 void initialize(Player * object,char * name, int hp, int exp, int x, int y){
-  object->name = name;
+  strcpy(object->name,name);
   object->hp = hp;
   object->exp = exp;
   object->x = x;
   object->y = y;
+}
+
+void stats(Player * p){
+   fprintf(stdout, "%s: location=(%u,%u), HP=%u, EXP=%u\n",
+          p->name, p->x, p->y, p->hp, p->exp);
 }
 
 int main(int argc, char* argv[]){
@@ -70,6 +80,7 @@ int main(int argc, char* argv[]){
 
   // Select
   fd_set readfds;
+  fd_set writefds;
 
   struct sockaddr_in sin;
   memset(&sin, 0, sizeof(sin));
@@ -100,31 +111,34 @@ int main(int argc, char* argv[]){
     return;
   }
 
-  printf("command >> ");
+  
 
-  while(!done){
-    
+  while(1){
     // Clear the set readfds;
     FD_ZERO(&readfds);
     FD_SET(STDIN, &readfds);
     FD_SET(sock, &readfds);
-
+      
     if (select(sock+1,&readfds,NULL,NULL,NULL) == -1){
       perror("select");
       return 0;
     }
+
+    /*
+     * Handling stdin
+     *
+     */
     
     if(FD_ISSET(STDIN, &readfds)){
-      // Handle stdin
-      
+      show_prompt();
       scanf("%s %s",command, arg);
 
       if (strcmp(command,"login") == 0){ // LOGIN
 	char* name = arg; // TODO: Sanity check the input.'
 	
-	// Do some checking here
-	self->name = arg;
+	strcpy(self->name,arg);
 	int status = handlelogin(name,sock);
+
 
       } else if(strcmp(command,"move") == 0){ // MOVE
 	char* direction = arg;
@@ -133,22 +147,26 @@ int main(int argc, char* argv[]){
 
 
       } else if(strcmp(command,"attack") == 0){ // ATTACK
-	char* victim = arg;
+	char* victimname = arg;
 
-	// Reprocessing
-
-	if (strcmp(self->name, victim)==0){
-	  return 0;
+	if(!check_player_name(victimname)){
+	  printf("! Invalid name: %s\n", victimname);
+	  continue;
 	}
 
 	// Search for the guy in the list
+	Player * victim = findPlayer(victimname,others);
 
-	// Check if he is in sight
+	if (!victim){ // If it is NULL
+	  on_not_visible();
+	  continue;
+	}
 
-	// Handling message deliever
+	int isvisible = isVisible(self->x,self->y,victim->x,victim->y);
 
-	if(strlen(victim) > 8){
-	  // Say that the string is too long
+	if(!isvisible){
+	  on_not_visible();
+	  continue;
 	}
 
 	int status = handleattack(victim,sock);
@@ -163,11 +181,34 @@ int main(int argc, char* argv[]){
 	
 	
       } else if(strcmp(command,"logout") == 0){
-	      handlelogout(self->name,sock);
-	done = 1;
+	if (handlelogout(self->name,sock) < 0){
+	  perror("handlelogout");
+	}
+	isLogin = 0; // No longer logged in
+
       } else {
 	printf("Unrecognized command.\n");
       }
+
+
+
+
+
+
+
+
+
+
+
+
+      /*
+       * Handling data from sock
+       *
+       */
+
+
+
+
 
     } else if (FD_ISSET(sock, &readfds)){ // Receiving from sock
       // Block and wait for response from the server
@@ -180,9 +221,9 @@ int main(int argc, char* argv[]){
       
       int j;
 
-      for(j = 0; j < read_bytes; j++){
-	printf("%02x ", buffer[j]);
-      }
+      /* for(j = 0; j < read_bytes; j++){ */
+/* 	printf("%02x ", buffer[j]); */
+/*       } */
       
       unsigned char header_c[HEADER_LENGTH];
       while(offset < read_bytes){
@@ -192,18 +233,26 @@ int main(int argc, char* argv[]){
 	// Copy the headers
 	offset += HEADER_LENGTH;
 
-	/* for(j = 0; j < HEADER_LENGTH; j++){ */
-/* 	  printf("%02x ", header_c[j]); */
-/* 	} */
+	for(j = 0; j < HEADER_LENGTH; j++){
+	  printf("%02x ", header_c[j]);
+	}
+
+	printf("\n");
 
 	// Cast it to struct header
 	struct header * hdr = (struct header *) header_c;
 
-	int payload_len = hdr->len-HEADER_LENGTH;
+	int payload_len = ntohs(hdr->len)-HEADER_LENGTH;
+
+	printf("payload_len: %d", payload_len);
 
 	char payload_c[payload_len];
 	for(j = 0; j < payload_len; j++){
 	  payload_c[j] = *(p+offset+j);
+	}
+
+	for(j = 0; j < HEADER_LENGTH; j++){
+	  printf("%02x ", header_c[j]);
 	}
 
 	offset += payload_len;
@@ -223,8 +272,8 @@ int main(int argc, char* argv[]){
 	    on_login_reply(lreply->error_code);
 
 	    if(lreply->error_code == 0){
-	      self->hp = ntohs(lreply->hp);
-	      self->exp = ntohs(lreply->exp);
+	      self->hp = ntohl(lreply->hp);
+	      self->exp = ntohl(lreply->exp);
 	      self->x = lreply->x;
 	      self->y = lreply->y;
 
@@ -234,27 +283,26 @@ int main(int argc, char* argv[]){
 	    }
 	  }
 	} else if(hdr->msgtype == MOVE_NOTIFY){
-	  printf("I got a move notify message.\n");
-	  for(j = 0; j < 20; j++){
-	    printf("%02x ", payload_c[j]);
-	  }
+	  /* printf("I got a move notify message.\n"); */
+/* 	  for(j = 0; j < 20; j++){ */
+/* 	    printf("%02x ", payload_c[j]); */
+/* 	  } */
+
+/* 	  printf("\n"); */
+
 	  struct move_notify * mn = (struct move_notify *) payload_c;
-	  // If the player is not insight, and the new location is not visible;
 	  Node *p;
 
-	  /* printf("name: %s.\n", mn->name); */
+	  /* stats(self); */
+
+/* 	  printf("name: %s.\n", mn->name); */
 /* 	  printf("hp: %x.\n", ntohl(mn->hp)); */
 /* 	  printf("exp: %x.\n", ntohl(mn->exp)); */
 /* 	  printf("x: %02x.\n", mn->x); */
 /* 	  printf("y: %02x.\n", mn->y); */
 
-/* 	  self->hp = ntohs(mn->hp); */
-/* 	  self->exp = ntohs(mn->exp); */
-/* 	  self->x = mn->x; */
-/* 	  self->y = mn->y; */
-
+	  stats(self);
 	  if (strcmp(mn->name,self->name)==0){ // If the guy is self
-	    printf("This message is for me.\n");
 	    self->hp = ntohl(mn->hp);
 	    self->exp = ntohl(mn->exp);
 	    self->x = mn->x;
@@ -264,14 +312,18 @@ int main(int argc, char* argv[]){
 
 	  } else {
 	    p = findPlayer(mn->name,others);
-	    printf("1\n");
 	    if(p == NULL){ // Not in the list
-	      // Make a new node
+	      
+	      // Adding the player
 	      Node * newnode = (Node*) malloc(sizeof(Node)); // TODO: remember to free this
+
 	      Player * newplayer = (Player*) malloc(sizeof(Player)); // TODO: remember to free this first
-	      printf("3\n");
-	      initialize(newplayer, mn->name, ntohl(mn->hp), ntohl(mn->exp), ntohs(mn->x), ntohs(mn->y));
-	      printf("4\n");
+
+	      strcpy(newplayer->name,mn->name);
+	      newplayer->hp = ntohl(mn->hp);
+	      newplayer->exp = ntohl(mn->exp);
+	      newplayer->x = mn->x;
+	      newplayer->y = mn->y;
 
 	      newnode->datum = newplayer;
 	      newnode->next = NULL;
@@ -279,46 +331,60 @@ int main(int argc, char* argv[]){
 	      if(tail == NULL){
 		others = newnode;
 		tail = newnode;
-		printf("5\n");
+	
 	      } else {
 		tail->next = newnode;
 		tail = newnode;
 	      }
-      	      printf("6\n");
-	      int oldp = abs((self->x - p->datum->x))<=5 
-		&& abs((self->y - p->datum->y))<=5; // if the old position is in sight
-	      int newp = abs((self->x - p->datum->x))<5 
-		&& abs((self->y - p->datum->y))<5; // if the new position is in sight
-	      printf("6\n");
+
+	      // Checking for vision
+	      int oldp = abs((self->x - newplayer->x))<=5 
+		&& abs((self->y - newplayer->y))<=5; // if the old position is in sight
+	      int newp = abs((self->x - newplayer->x))<5 
+		&& abs((self->y - newplayer->y))<5; // if the new position is in sight
+
 	      if (oldp || newp){
 		if (newp){
-		  on_move_notify(p->datum->name, p->datum->x, p->datum->y, p->datum->hp,p->datum->exp);
-		  printf("6\n");
-		}
-	      }
-	    }
+		  on_move_notify(newplayer->name, newplayer->x, newplayer->y, newplayer->hp,newplayer->exp);
+		} // End of if
+	      } // End of if
+
+	    } // End of if p == NULL
 	  }
 	  
 	} else if(hdr->msgtype == ATTACK_NOTIFY){
-	  struct attack_notify * an = (struct attack_notify *)buffer;
-	  Player * attacker,victim;
-	  //set x,y for attacker and victim
-	  /* foreach (@others){ */
-	  /* 		  if (strcmp(an->attacker_name,$_->name) == 0){ */
-	  /* 			  attacker->x = $_->x; */
-	  /* 			  attacker->y = $_->y; */
+	  struct attack_notify * an = (struct attack_notify *)payload_c;
+	  Player * att = findPlayer(an->attacker_name, others);
+	  Player * vic = findPlayer(an->victim_name, others);
 
+	  int updated_hp = ntohl(an->hp);
+	  char damage = an->damage;
+	  // Check in the case where they are null
 
+	  // Check the visibility
+	  int attVisible = isVisible(self->x,self->y,att->x,att->y);
+	  int vicVisible = isVisible(self->x,self->y,vic->x,vic->y);
 
-	  //bool attacker_in_sight = abs((self->x - p->x))<=5 && abs((self->y - p->y))<=5;
-
-	  //bool victim_in_sight = abs((self->x - p->x))<=5 && abs((self->y - p->y))<=5;
-
-	  break;
+	  if (attVisible && vicVisible){
+	    on_attack_notify(att->name,vic->name,damage,updated_hp);
+	  }
+	  
+	  
 	} else if(hdr->msgtype == SPEAK_NOTIFY){ //
+		struct speak_notify* sreply = (struct speak_notify*) payload_c;
+		unsigned char * message = sreply->msg;
+		unsigned char * name = sreply->broadcaster;
+
+		// null terminated & no longer than 255
+		if(!check_player_message(message)){ 		//TODO: need to check this in the send too!!
+		  printf("! Invalid format\n");
+		  continue;
+		}
+		
+		printf("%s: %s\n",name,message);
 	} else if(hdr->msgtype == LOGOUT_NOTIFY){
-//		struct logout_reply * loreply = (struct logout_reply *) payload_c;
-//		printf("Player %s has left the tiny world of warcraft.\n",loreply->name);	  	
+		struct logout_reply * loreply = (struct logout_reply *) payload_c;
+		printf("Player %s has left the tiny world of warcraft.\n",loreply->name);
 	}
       }
     }
--- bin/sandbox.c
+++ bin/sandbox.c
@@ -1,36 +1,12 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-typedef struct P{
-  int x;
-} Player;
-
-struct list_el {
-   Player *  val;
-   struct list_el * next;
-};
-
-typedef struct list_el item;
-
-void main() {
-   item * curr, * head;
-   int i;
-
-   head = NULL;
-
-   for(i=1;i<=10;i++) {
-      curr = (item *)malloc(sizeof(item));
-      Player * p = (Player*) malloc(sizeof(Player));
-      p->x = i;
-      curr->val = p;
-      curr->next  = head;
-      head = curr;
-   }
-
-   curr = head;
-
-   while(curr) {
-      printf("%d\n", curr->val->x);
-      curr = curr->next ;
-   }
+int main(){
+  char * s = "login phuc";
+  char command[80] = "login phuc";
+  char arg[80];
+  
+  sscanf(command,"%s %s", command,arg);
+  puts(command);
+  printf("%s\n", arg);
 }
