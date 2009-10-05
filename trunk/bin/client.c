b59212cd5671e40c04eac614b906e9036d5321b1
Nguyen.Phuc.X <Nguyen.Phuc.X@6a860f62-a4c3-11de-afd7-c7ca60a6e2ec>
2009-10-05 03:12:01 +0000

    Finished the login process

--- trunk/bin/client.c
+++ trunk/bin/client.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "constants.h"
 #include <sys/types.h>
@@ -9,16 +10,10 @@
 
 #include "header.h"
 #include "helper.h"
+#include "messages.h"
 
 #define STDIN 0
-
-
-struct list_el {
-  int val;
-  struct list_el * next;
-};
-
-typedef struct list_el Node;
+#define HEADER_LENGTH 4
 
 typedef struct P{
   char * name;
@@ -28,13 +23,41 @@ typedef struct P{
   int y;
 } Player;
 
+struct list_el {
+  Player * datum;
+  struct list_el * next;
+};
+
+typedef struct list_el Node;
+
+Node * findPlayer(char * name, Node * list){
+  Node * p;
+  for(p = list; p != NULL; p = p->next){
+    if (strcmp(p->datum->name,name)==0){
+      return p;
+    }
+  }
+  return NULL;
+}
+
+void initialize(Player * object,char * name, int hp, int exp, int x, int y){
+  object->name = name;
+  object->hp = hp;
+  object->exp = exp;
+  object->x = x;
+  object->y = y;
+}
+
 int main(int argc, char* argv[]){
 
   // Model Variables
   Player * self = (Player *) malloc(sizeof(Player)); // Remember to free this
   Node * others;
+  Node * tail;
 
-  bool isLogin = false;
+  Node * p;
+
+  int isLogin = 0;
   char command[80];
   char arg[80];
 
@@ -61,15 +84,10 @@ int main(int argc, char* argv[]){
   serverIP = argv[2];
   serverPort = atoi(argv[4]);
 
-  printf("serverIP: %s\n", serverIP);
-  printf("serverPort: %d\n", serverPort);
-
   sock = socket(AF_INET, SOCK_STREAM, 0);
   if(sock < 0){
     perror("socket() failed\n");
     abort();
-  }else{
-    printf("socket established: sock = %d\n", sock);
   }
 
   sin.sin_family = AF_INET;
@@ -80,11 +98,8 @@ int main(int argc, char* argv[]){
     close(sock);
     perror("connection failed");
     return;
-  }else{
-    printf("Connection esablished\n");
   }
 
-  printf("Hello Welcome to tiny World of Warcraft!\n");
   printf("command >> ");
 
   while(!done){
@@ -93,6 +108,7 @@ int main(int argc, char* argv[]){
     FD_ZERO(&readfds);
     FD_SET(STDIN, &readfds);
     FD_SET(sock, &readfds);
+
     if (select(sock+1,&readfds,NULL,NULL,NULL) == -1){
       perror("select");
       return 0;
@@ -103,17 +119,12 @@ int main(int argc, char* argv[]){
       
       scanf("%s %s",command, arg);
 
-      printf("command: %s\n arg: %s\n", command,arg);
-
       if (strcmp(command,"login") == 0){ // LOGIN
 	char* name = arg; // TODO: Sanity check the input.'
 	
 	// Do some checking here
 	self->name = arg;
 	int status = handlelogin(name,sock);
-
-
-
 
       } else if(strcmp(command,"move") == 0){ // MOVE
 	char* direction = arg;
@@ -154,94 +165,147 @@ int main(int argc, char* argv[]){
       } else if(strcmp(command,"logout") == 0){
 	done = 1;
       } else {
-	printf("WTF?\n");
+	printf("Unrecognized command.\n");
       }
-
-
-
 
     } else if (FD_ISSET(sock, &readfds)){ // Receiving from sock
       // Block and wait for response from the server
       unsigned char buffer[4096];
       int expected_data_len = sizeof(buffer);
+      unsigned char *p = (char*) buffer; // Introduce a new pointer
+      int offset = 0;
+
       int read_bytes = recv(sock,buffer,expected_data_len,0);
-
-      printf("buffer's len: %d\n", sizeof(buffer));
-
+      
       int j;
-      char header_c[4];
-      for(j = 0; j < 4; j++){
-	header_c[j] = buffer[j];
+
+      for(j = 0; j < read_bytes; j++){
+	printf("%02x ", buffer[j]);
       }
+      
+      unsigned char header_c[HEADER_LENGTH];
+      while(offset < read_bytes){
+	for(j = 0; j < HEADER_LENGTH; j++){
+	  header_c[j] = *(p+offset+j);
+	}
+	// Copy the headers
+	offset += HEADER_LENGTH;
 
-      for(j = 0; j < 4; j++){
-	printf("%02x ", header_c[j]);
-      }
+	/* for(j = 0; j < HEADER_LENGTH; j++){ */
+/* 	  printf("%02x ", header_c[j]); */
+/* 	} */
 
-      struct header *  hdr = (struct header *) header_c;
+	// Cast it to struct header
+	struct header * hdr = (struct header *) header_c;
 
-      printf("Received a message.\n");
-      printf("msgtype: %x\n", hdr->msgtype);
+	int payload_len = hdr->len-HEADER_LENGTH;
 
-      // Check for the msgtype
-      if(m->msgtype == LOGIN_REPLY){
+	char payload_c[payload_len];
+	for(j = 0; j < payload_len; j++){
+	  payload_c[j] = *(p+offset+j);
+	}
 
-	printf("Message is a login reply\n");
+	offset += payload_len;
+      
+	// @TODO: Check for the version
+
+	// Check for the msgtype
+	if(hdr->msgtype == LOGIN_REPLY){
 	  // LOGIN_REPLY
 
-	  struct login_reply * lreply = (struct login_reply *) buffer;
+	  struct login_reply * lreply = (struct login_reply *) payload_c;
 	  if(isLogin){
 	    // Treat it as a malformed package
+	    on_malformed_message_from_server();
+
 	  } else {
+	    on_login_reply(lreply->error_code);
+
 	    if(lreply->error_code == 0){
-	      // use message.h
 	      self->hp = ntohs(lreply->hp);
 	      self->exp = ntohs(lreply->exp);
-	      self->x = ntohs(lreply->x);
-	      self->y = ntohs(lreply->y);
+	      self->x = lreply->x;
+	      self->y = lreply->y;
 
-	      isLogin = true;
+	      isLogin = 1;
 	    } else if (lreply->error_code == 1){
-	      // use message.h
+	      
 	    }
 	  }
-      } else if(m->msgtype == MOVE_NOTIFY){
-	  struct move_notify * mn = (struct move_notify *) buffer;
+	} else if(hdr->msgtype == MOVE_NOTIFY){
+	  printf("I got a move notify message.\n");
+	  for(j = 0; j < 20; j++){
+	    printf("%02x ", payload_c[j]);
+	  }
+	  struct move_notify * mn = (struct move_notify *) payload_c;
 	  // If the player is not insight, and the new location is not visible;
-	  
-	  Player * p;
-	  bool found = false;
+	  Node *p;
 
-	  // Find the dude with the same name
+	  /* printf("name: %s.\n", mn->name); */
+/* 	  printf("hp: %x.\n", ntohl(mn->hp)); */
+/* 	  printf("exp: %x.\n", ntohl(mn->exp)); */
+/* 	  printf("x: %02x.\n", mn->x); */
+/* 	  printf("y: %02x.\n", mn->y); */
 
-	  if(!found){
-	    // Add the dude to the array
-	    // Call the dude p
+/* 	  self->hp = ntohs(mn->hp); */
+/* 	  self->exp = ntohs(mn->exp); */
+/* 	  self->x = mn->x; */
+/* 	  self->y = mn->y; */
 
-	    //Player * p;
-	  }
-	  
-	  bool oldp = abs((self->x - p->x))<=5 && abs((self->y - p->y))<=5; // if the old position is in sight
-	  bool newp = abs((self->x - p->x))<5 && abs((self->y - p->y))<5; // if the new position is in sight
-	  if (oldp || newp){
-	    // update his stat
-	    if (newp){
-	      // send out a message
-	    }
-	    // in either case, update the position for player p in database.
+	  if (strcmp(mn->name,self->name)==0){ // If the guy is self
+	    printf("This message is for me.\n");
+	    self->hp = ntohl(mn->hp);
+	    self->exp = ntohl(mn->exp);
+	    self->x = mn->x;
+	    self->y = mn->y;
+
+	    on_move_notify(self->name, self->x, self->y, self->hp,self->exp);
+
 	  } else {
-	    // Ignore
+	    p = findPlayer(mn->name,others);
+	    printf("1\n");
+	    if(p == NULL){ // Not in the list
+	      // Make a new node
+	      Node * newnode = (Node*) malloc(sizeof(Node)); // TODO: remember to free this
+	      Player * newplayer = (Player*) malloc(sizeof(Player)); // TODO: remember to free this first
+	      printf("3\n");
+	      initialize(newplayer, mn->name, ntohl(mn->hp), ntohl(mn->exp), ntohs(mn->x), ntohs(mn->y));
+	      printf("4\n");
+
+	      newnode->datum = newplayer;
+	      newnode->next = NULL;
+
+	      if(tail == NULL){
+		others = newnode;
+		tail = newnode;
+		printf("5\n");
+	      } else {
+		tail->next = newnode;
+		tail = newnode;
+	      }
+      	      printf("6\n");
+	      int oldp = abs((self->x - p->datum->x))<=5 
+		&& abs((self->y - p->datum->y))<=5; // if the old position is in sight
+	      int newp = abs((self->x - p->datum->x))<5 
+		&& abs((self->y - p->datum->y))<5; // if the new position is in sight
+	      printf("6\n");
+	      if (oldp || newp){
+		if (newp){
+		  on_move_notify(p->datum->name, p->datum->x, p->datum->y, p->datum->hp,p->datum->exp);
+		  printf("6\n");
+		}
+	      }
+	    }
 	  }
-	  break;
 	  
-      } else if(m->msgtype == ATTACK_NOTIFY){
-	  struct attack_notify * an = (struct attack_notify *) m->buffer;
+	} else if(hdr->msgtype == ATTACK_NOTIFY){
+	  struct attack_notify * an = (struct attack_notify *)buffer;
 	  Player * attacker,victim;
 	  //set x,y for attacker and victim
 	  /* foreach (@others){ */
-/* 		  if (strcmp(an->attacker_name,$_->name) == 0){ */
-/* 			  attacker->x = $_->x; */
-/* 			  attacker->y = $_->y; */
+	  /* 		  if (strcmp(an->attacker_name,$_->name) == 0){ */
+	  /* 			  attacker->x = $_->x; */
+	  /* 			  attacker->y = $_->y; */
 
 
 
@@ -250,8 +314,9 @@ int main(int argc, char* argv[]){
 	  //bool victim_in_sight = abs((self->x - p->x))<=5 && abs((self->y - p->y))<=5;
 
 	  break;
-      } else if(m->msgtype == SPEAK_NOTIFY){ //
-      } else if(m->msgtype == LOGOUT_NOTIFY){
+	} else if(hdr->msgtype == SPEAK_NOTIFY){ //
+	} else if(hdr->msgtype == LOGOUT_NOTIFY){
+	}
       }
     }
   }
