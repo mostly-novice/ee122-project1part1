90f19cdf8c4c0886c73c9d4e9c3eba29fb12ddf7
Nguyen.Phuc.X <Nguyen.Phuc.X@6a860f62-a4c3-11de-afd7-c7ca60a6e2ec>
2009-10-22 03:47:21 +0000

    Server code and plan

---
+++ servercode/sandbox.py
@@ -0,0 +1,63 @@
+# We are going to keep a list of players that are active
+
+# Login_request
+
+# First you check if the guy is login by searching through the active clients
+handleLoginRequest
+if the guy is already logged in:
+    send a invalid state
+else:
+    make a new instance of Player
+    check if the file with that name exists
+    if name does not exist:
+        open a new file
+        randomize stats
+        write stats to the file
+        initialize the player
+    else if name exists:
+        open an existed file
+        read from the file and initialize the player
+    send LOGIN_REPLY to the player
+    send MOVE_NOTIFY to the rest of the players
+
+# Do we need to write to server every 
+
+def handleMove(player,direction):
+    if direction == NORTH:
+        p.y += 3
+    elif direction == SOUTH:
+        p.y -= 3
+    elif direction == WEST:
+        p.x -= 3
+    elif direction == EAST:
+        p.x += 3
+
+    # Either write to this here or when the server exits
+    write to file here
+    
+    broadcast MOVE_NOTIFY
+
+def handleAttack(attacker,victim):
+    if the attacker is the same as the victim:
+        ignore this message
+    if there is no such victim:
+        ignore this message
+    damage = random(10,20)
+    if damage > victim.hp:
+        damage = victim.hp
+
+    victim.hp -= damage
+    attack.exp += damage
+
+    broadcast ATTACK_NOTIFY with attacker.name,victim.name,damage,victim.hp
+
+    if victim.hp == 0:
+        victim.hp = random(30-50);
+        vicctim.x = random(0,99);
+        victim.y = random(0,99);
+    
+def handleSpeak(player,message):
+    broadcast(SPEAK_NOTIFY,name,message);
+
+handleLoginReply
+Form the packets and send them
