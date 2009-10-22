c4857a8153398d7e414a6b8670106a843056be42
Nguyen.Phuc.X <Nguyen.Phuc.X@6a860f62-a4c3-11de-afd7-c7ca60a6e2ec>
2009-10-22 03:51:33 +0000

    Added sendhelper

---
+++ trunk/servercode/sendhelper.h
@@ -0,0 +1,18 @@
+// Helper to send messages.
+int sendloginreply(){
+}
+
+int sendmovenotify(){
+}
+
+int sendattacknotify(){
+}
+
+int sendspeaknotify(){
+}
+
+int sendlogoutnotify(){
+}
+
+int sendinvalidstate(){
+}
