1a7686a50a9e82e4194207995f4959f97380c572
firedhippy <firedhippy@6a860f62-a4c3-11de-afd7-c7ca60a6e2ec>
2009-11-02 07:54:49 +0000

    fixed headers

--- trunk/trackercode/header.h
+++ trunk/trackercode/header.h
@@ -7,7 +7,10 @@ struct header {
 // Payload Type
 struct login_request {
   unsigned char name[10];
-  unsigned char padding[2];
+  unsigned int hp;
+  unsigned int exp;
+  unsigned char x;
+  unsigned char y;
 }__attribute__((packed));
 
 struct logout_reply{
@@ -63,9 +66,85 @@ struct speak_notify {
 
 struct logout_notify {
   unsigned char name[10];
-  unsigned char padding[2];
+  unsigned int hp;
+  unsigned int exp;
+  unsigned char x;
+  unsigned char y;
 }__attribute__((packed));
 
 struct invalid_state {
   unsigned char error_code;
 }__attribute__((packed));
+
+struct storage_location_request {
+    unsigned char message_type;
+    unsigned int ID;
+    unsigned char name[10];
+    unsigned char padding;
+}__attribute__((packed));
+
+struct storage_location_response {
+    unsigned char message_type;
+    unsigned int ID;
+    unsigned int server_ip;
+    unsigned short port;
+    unsigned char paddding;
+}__attribute__((packed));
+
+struct server_area_request {
+    unsigned char message_type;
+    unsigned int ID;
+    unsigned char x;
+    unsigned char y;
+    unsigned char paddding;
+}__attribute__((packed));
+
+struct server_area_response {
+    unsigned char message_type;
+    unsigned int ID;
+    unsigned int server_ip;
+    unsigned short port;
+    unsigned char max_x;
+    unsigned char max_y;
+    unsigned char min_x;
+    unsigned char min_y;
+    unsigned char paddding;
+}__attribute__((packed));
+
+struct player_state_request {
+    unsigned char message_type;
+    unsigned int ID;
+    unsigned char name[10];
+    unsigned char paddding;
+}__attribute__((packed));
+
+struct player_state_response {
+    unsigned char message_type;
+    unsigned int ID;
+    unsigned char name[10];
+    unsigned int hp;
+    unsigned int exp;
+    unsigned char x;
+    unsigned char y;
+    unsigned char paddding[3];
+}__attribute__((packed));
+
+struct save_state_request {
+    unsigned char message_type;
+    unsigned int ID;
+    unsigned char name[10];
+    unsigned int hp;
+    unsigned int exp;
+    unsigned char x;
+    unsigned char y;
+    unsigned char paddding[3];
+}__attribute__((packed));
+
+struct save_state_response {
+    unsigned char message_type;
+    unsigned int ID;
+    unsigned char error_code;
+    unsigned short padding;
+}__attribute__((packed));
+
+
