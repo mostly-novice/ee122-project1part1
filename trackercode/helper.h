899a0098f903cb9e42d3b80d9faf8f07b4e9e887
firedhippy <firedhippy@6a860f62-a4c3-11de-afd7-c7ca60a6e2ec>
2009-11-03 04:42:19 +0000

    added some coments is all

--- trackercode/helper.h
+++ trackercode/helper.h
@@ -3,76 +3,76 @@
 #include "constants.h"
 
 void printMessage(char * message, int len){
-  int i;
-  for(i = 0; i < len; i++){
-    printf("%02x ", *(message+i));
-  }
-  printf("\n");
+    int i;
+    for(i = 0; i < len; i++){
+	printf("%02x ", *(message+i));
+    }
+    printf("\n");
 }
 
 //Helper: handle the various task in clients
 // TESTED
 int handlelogin(char* name,int sock){
-  int i = 0;
-  int j;
-  // Header
-  struct header *hdr = (struct header *) malloc(sizeof(int)); // remember to free this
-  struct login_request * payload = (struct login_request *) malloc(sizeof(int)*3); // remember to free this
-  hdr->version = 0x04;
-  hdr->len = htons(0x0010);
-  hdr->msgtype = 0x1;
-  strcpy(payload->name,name);
-  unsigned char * payload_c = (unsigned char*) payload;
-  unsigned char * header_c = (unsigned char *) hdr;
-  unsigned char tosent[16];
+    int i = 0;
+    int j;
+    // Header
+    struct header *hdr = (struct header *) malloc(sizeof(int)); // remember to free this
+    struct login_request * payload = (struct login_request *) malloc(sizeof(int)*3); // remember to free this
+    hdr->version = 0x04;
+    hdr->len = htons(0x0010);
+    hdr->msgtype = 0x1;
+    strcpy(payload->name,name);
+    unsigned char * payload_c = (unsigned char*) payload;
+    unsigned char * header_c = (unsigned char *) hdr;
+    unsigned char tosent[16];
 
-  for(j = 0; j < 16; j++){
-    if(j<4) tosent[j] = header_c[j];
-    else tosent[j] = payload_c[j-4];
-  }
+    for(j = 0; j < 16; j++){
+	if(j<4) tosent[j] = header_c[j];
+	else tosent[j] = payload_c[j-4];
+    }
 
-  // Send a login message to the server
-  int bytes_sent = send(sock, tosent,16,0);
-  if (bytes_sent < 0) perror("send failed");
-  free(hdr);
-  free(payload);
-  return 0;
+    // Send a login message to the server
+    int bytes_sent = send(sock, tosent,16,0);
+    if (bytes_sent < 0) perror("send failed");
+    free(hdr);
+    free(payload);
+    return 0;
 }
 
 // TESTED
 int handlemove(unsigned char d, int sock){
-  struct header *hdr = (struct header *) malloc(sizeof(int)); // Remember to free this
-  struct move * payload = (struct move *) malloc(sizeof(int)); // Remember to free this
-  int j;
+    struct header *hdr = (struct header *) malloc(sizeof(int)); // Remember to free this
+    struct move * payload = (struct move *) malloc(sizeof(int)); // Remember to free this
+    int j;
 
-  hdr->version = 0x4;
-  hdr->len = htons(0x08);
-  hdr->msgtype = MOVE;
+    hdr->version = 0x4;
+    hdr->len = htons(0x08);
+    hdr->msgtype = MOVE;
 
-  payload->direction = d;
+    payload->direction = d;
 
-  unsigned char * payload_c = (unsigned char*) payload;
-  unsigned char * header_c = (unsigned char *) hdr;
-  unsigned char tosent[8];
+    unsigned char * payload_c = (unsigned char*) payload;
+    unsigned char * header_c = (unsigned char *) hdr;
+    unsigned char tosent[8];
 
-  for(j = 0; j < 8; j++){
-    if(j<4){
-      tosent[j] = header_c[j];
-    } else {
-      tosent[j] = payload_c[j-4];
+    for(j = 0; j < 8; j++){
+	if(j<4){
+	    tosent[j] = header_c[j];
+	} else {
+	    tosent[j] = payload_c[j-4];
+	}
     }
-  }
-  
-  int bytes_sent = send(sock,tosent,8,0);
-  if (bytes_sent < 0){
-    perror("send failed.\n");
-  }
 
-  // Freeing the pointers;
-  free(hdr);
-  free(payload);
+    int bytes_sent = send(sock,tosent,8,0);
+    if (bytes_sent < 0){
+	perror("send failed.\n");
+    }
 
-  return 0;
+    // Freeing the pointers;
+    free(hdr);
+    free(payload);
+
+    return 0;
 }
 
 
@@ -80,108 +80,117 @@ int handlemove(unsigned char d, int sock){
 // Assuming that the input is valid
 // @TODO: Test
 int handleattack(char * victim, int sock){
-  int j = 0;
+    int j = 0;
 
-  // Sending message
-  struct header *hdr = (struct header *) malloc(sizeof(int)); // remember to free this
-  struct attack * payload = (struct attack *) malloc(sizeof(int)*3); // remember to free this
+    // Sending message
+    struct header *hdr = (struct header *) malloc(sizeof(int)); // remember to free this
+    struct attack * payload = (struct attack *) malloc(sizeof(int)*3); // remember to free this
 
-  hdr->version = 0x4;
-  hdr->len = htons(0x10);
-  hdr->msgtype = ATTACK;
+    hdr->version = 0x4;
+    hdr->len = htons(0x10);
+    hdr->msgtype = ATTACK;
 
-  // Copy the name
-  strcpy(payload->victimname,victim);
+    // Copy the name
+    strcpy(payload->victimname,victim);
 
-  unsigned char * payload_c = (unsigned char*) payload;
-  unsigned char * header_c = (unsigned char *) hdr;
+    unsigned char * payload_c = (unsigned char*) payload;
+    unsigned char * header_c = (unsigned char *) hdr;
 
-  unsigned char tosent[16];
+    unsigned char tosent[16];
 
-  for(j = 0; j < 16; j++){
-    if(j<4){
-      tosent[j] = header_c[j];
-    } else {
-      tosent[j] = payload_c[j-4];
+    for(j = 0; j < 16; j++){
+	if(j<4){
+	    tosent[j] = header_c[j];
+	} else {
+	    tosent[j] = payload_c[j-4];
+	}
     }
-  }
 
-  // Sending
-  int bytes_sent = send(sock,tosent,16,0);
-  if (bytes_sent < 0){
-    perror("send failed.\n");
-    return -1;
-  } else {
-    //printf("Send: %d bytes\n", bytes_sent);
-  }
+    // Sending
+    int bytes_sent = send(sock,tosent,16,0);
+    if (bytes_sent < 0){
+	perror("send failed.\n");
+	return -1;
+    } else {
+	//printf("Send: %d bytes\n", bytes_sent);
+    }
 
-  free(hdr);
-  free(payload);
-  return 0;
+    free(hdr);
+    free(payload);
+    return 0;
 }
 
 // Speak
 int handlespeak(char * m, int sock){
- // Header
+    // Header
 
-  unsigned int payloadLength = strlen(m) + 1 + 4 - ((strlen(m)+1) % 4);
-  struct header *hdr = (struct header *) malloc(sizeof(int)); // remember to free this
+    unsigned int payloadLength = strlen(m) + 1 + 4 - ((strlen(m)+1) % 4);
+    struct header *hdr = (struct header *) malloc(sizeof(int)); // remember to free this
 
-  //msg = (char *) malloc(sizeof(char)*(strlen(m)+1));
+    //msg = (char *) malloc(sizeof(char)*(strlen(m)+1));
 
-  int j;
+    int j;
 
-  unsigned int totalMessageLength = payloadLength + 4;
+    unsigned int totalMessageLength = payloadLength + 4;
 
-  hdr->version = 0x4;
-  hdr->len = htons(totalMessageLength);
-  hdr->msgtype = SPEAK;
+    hdr->version = 0x4;
+    hdr->len = htons(totalMessageLength);
+    hdr->msgtype = SPEAK;
 
-  unsigned char * header_c = (unsigned char *) hdr;
+    unsigned char * header_c = (unsigned char *) hdr;
 
-  unsigned char tosent[totalMessageLength];
+    unsigned char tosent[totalMessageLength];
 
-  for(j = 0; j < totalMessageLength; j++){
-    if(j<4){
-      tosent[j] = header_c[j];
-    } else {
-      tosent[j] = *(m+j-4);
+    for(j = 0; j < totalMessageLength; j++){
+	if(j<4){
+	    tosent[j] = header_c[j];
+	} else {
+	    tosent[j] = *(m+j-4);
+	}
     }
-  }
 
-  // Send a speak message to the server
-  int bytes_sent = send(sock, tosent,totalMessageLength,0);
-  if (bytes_sent < 0) {
-    perror("send failed");
-  } else {
-    //printf("Sent: %d bytes\n", bytes_sent);
-  }
+    // Send a speak message to the server
+    int bytes_sent = send(sock, tosent,totalMessageLength,0);
+    if (bytes_sent < 0) {
+	perror("send failed");
+    } else {
+	//printf("Sent: %d bytes\n", bytes_sent);
+    }
 
-  free(hdr);
-  return 0;
+    free(hdr);
+    return 0;
 }
 
 int handlelogout(char * name,int sock){
-  struct header *hdr = (struct header *) malloc(sizeof(int));
-  int j;
+    struct header *hdr = (struct header *) malloc(sizeof(int));
+    int j;
 
-  hdr->version = 0x4;
-  hdr->len = htons(0x4);
-  hdr->msgtype = LOGOUT;
+    hdr->version = 0x4;
+    hdr->len = htons(0x4);
+    hdr->msgtype = LOGOUT;
 
-  char * header_c = (char*) hdr;
-  unsigned char tosent[4];
+    char * header_c = (char*) hdr;
+    unsigned char tosent[4];
 
-  for(j = 0; j < 4; j++){
-      tosent[j] = header_c[j];
-  }
+    for(j = 0; j < 4; j++){
+	tosent[j] = header_c[j];
+    }
 
-  // Sending the message;
-  int bytes_sent = send(sock,tosent,4,0);
-  if (bytes_sent < 0){
-    perror("send failed.\n");
+    // Sending the message;
+    int bytes_sent = send(sock,tosent,4,0);
+    if (bytes_sent < 0){
+	perror("send failed.\n");
+	free(hdr);
+    }
     free(hdr);
-  }
-  free(hdr);
 }
 
+unsigned int send_storage_location_request(char* name, unsigned int IP, unsigned short port) {
+    struct header *hdr = (struct header *) malloc(sizeof(int));
+    struct storage_location_request *slr = (storage_location_request *) malloc (sizeof storage_location_request);
+    hdr->len = htons(0x14); // 16 + 4 = 20
+    hdr->msgtype = STORAGE_LOCATION_REQUEST; // TODO: Add STORAGE_LOCATION_REQUEST to constants.h
+    slr->message_type = 0x0;
+    slr->ID = unique_identifier; // TODO: create unique_identifier
+    strcpy(name,slr->name);
+}
