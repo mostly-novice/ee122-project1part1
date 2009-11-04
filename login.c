c08b27d10b06b6520e06fadbfbf3606f30eb6382
Nguyen.Phuc.X <Nguyen.Phuc.X@6a860f62-a4c3-11de-afd7-c7ca60a6e2ec>
2009-11-04 18:41:31 +0000

    More cleaning up

--- login.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-//https://ee122-project1part1.googlecode.com/svn/trunk --username firedhippy
-//youll be prompted for pw located @@ http://code.google.com/p/ee122-project1part1/
-
-
-
