d72cca90510e5b4c97a80b295014e02f0d004931
Nguyen.Phuc.X <Nguyen.Phuc.X@6a860f62-a4c3-11de-afd7-c7ca60a6e2ec>
2009-11-19 02:39:39 +0000

    hasdf

--- proj2b/servercode/p2pheader.h
+++ proj2b/servercode/p2pheader.h
@@ -1,6 +1,36 @@
 struct header {
-  unsigned char version;
-  unsigned short len;
-  unsigned char msgtype;
+				unsigned char version;
+				unsigned short len;
+				unsigned char msgtype;
 }__attribute__((packed));
-__attribute__((packed));
+
+struct p2p_join_request {
+				unsigned char MsgVer;
+				unsigned short MsgLength;
+				unsigned char MsgType;
+				unsigned int P2P_ID;
+}__attribute__((packed));
+
+struct p2p_join_response {
+				unsigned char MsgVer;
+				unsigned short MsgLength;
+				unsigned char MsgType;
+				unsigned int UserNumber;
+				unsigned char* UserDataArray;
+}__attribute__((packed));
+
+struct p2p_bkup_request {
+				unsigned char MsgVer;
+				unsigned short MsgLength;
+				unsigned char MsgType;
+				unsigned char UserData[20];
+}__attribute__((packed));
+
+struct p2p_bkup_response {
+				unsigned char MsgVer;
+				unsigned short MsgLength;
+				unsigned char MsgType;
+				unsigned char ErrorCode;
+}__attribute__((packed));
+
+
